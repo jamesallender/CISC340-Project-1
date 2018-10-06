459689a485d796c33ff35330a31a15c1b0681f14
James <jamesa52416@gmail.com>
2018-10-06 11:08:47 -0500

    6

--- James Testing/assemblerJamesWorking.c
+++ James Testing/assemblerJamesWorking.c
@@ -107,8 +107,8 @@ int main(int argc, char **argv){
 	/*		SECOND PASS	OVER FILE 	*/
 	inFile = fopen(inFileName, "r");
 
-	int testPrint = GPOINTER_TO_INT(g_hash_table_lookup(hash, "start"));
-			printf ("\n\n\n\naddress: %d\n\n\n\n"testPrint);  
+	//int testPrint = GPOINTER_TO_INT(g_hash_table_lookup(hash, "start"));
+	//		printf ("\n\n\n\naddress: %d\n\n\n\n"testPrint);  
 
 	char *lineArr[4]; // array to hold the split elements of our line
 
