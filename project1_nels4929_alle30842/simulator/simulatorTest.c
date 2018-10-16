f20304e19fc0e22bfe121a330e7a8ae329cf5c09
James <jamesa52416@gmail.com>
2018-10-16 16:17:47 -0500

    ;

---
+++ project1_nels4929_alle30842/simulator/simulatorTest.c
@@ -0,0 +1,210 @@
+/* 
+James Allender
+Mitchell Nelson
+CISC-340 
+Project 01
+*/
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <math.h>
+#include <glib.h>
+#include <string.h>
+#include <ctype.h>
+#define NUMREG 8
+#define NUMMEMORY 65536
+// Structure for
+typedef struct state_struct {
+	int pc;
+	int mem[NUMMEMORY];
+	int reg[NUMREG];
+	int num_memory;
+} statetype;
+
+int convert_num(int num);
+void print_state(statetype *stateptr);
+void print_stats(int n_instrs);
+
+int main(int argc, char **argv){
+	// Variables
+	statetype state;
+	int opt; // the value of the opt form getopt
+	FILE* inFile; // our input File
+	char *inFileName; // our input file name
+	int iFlag = 0; // Flag to tell us if an i option (requiered) has been given
+	int writeToFileFlag = 0;
+	extern char *optarg; // The arguemnt to a opt - used by external
+	extern int optind; // the int id of the curent opt location - used by external
+	int lineInt;
+	int lineArr[4];
+	int haltFlag =0;
+	int n_instrs =0;
+	// Verify correct # of args given either 3 or 5 for 1 or 2 options (i,o)
+	if (argc != 3) {
+	       fprintf(stderr, "Was not given either 1 or 2 sets of input arguments.\nShould be:\t-i [input file]\nExiting\n");
+	       exit(EXIT_FAILURE);
+        }
+
+    // go through the args and get options
+	while ((opt = getopt(argc, argv, "i:o:")) != -1){
+		switch (opt){
+			case 'i':
+				inFileName = optarg;
+				iFlag = 1;
+				break;
+			default:
+            	fprintf(stderr, "Was given an unexpected argument, was given %d.\n Expected -i [input file]\nExiting\n", opt);
+               	exit(EXIT_FAILURE);
+       }
+	}
+	// Check to make sure we were given an input file
+	if (iFlag == 0) {
+       fprintf(stderr, "Expected minimum option -i\nNot Provided\nExiting\n");
+       exit(EXIT_FAILURE);
+    } // if
+
+
+	/*-------------------------PROCESS FILE------------------------------*/
+
+	inFile = fopen(inFileName, "r");
+	char lineBuffer[100]; // Array to hold our line
+	// Loop through the lines of the file a second timei
+	int i = 0;
+	while (fgets(lineBuffer, 100, inFile) !=NULL){
+	
+		lineInt  = strtol(lineBuffer,NULL,10);
+		
+		state.mem[i] = lineInt;
+		
+		state.num_memory++;	
+		
+		i++;
+	}	
+	state.pc =0;
+
+	while (1){
+		
+	
+		// "add" 0
+		// "nand" 1
+		// "lw" 2
+		// "sw"  3
+		// "beq" 4
+		// "jalr" 5
+		// "halt" 6
+		// "noop" 7;
+
+
+		int optCode = state.mem[state.pc] & 0x1C00000;
+
+		optCode = optCode >> 22;
+	
+		if (optCode == 6){
+			n_instrs ++;
+			break;
+		}
+	
+		int regA = state.mem[state.pc] & 0x380000;
+
+		regA = regA >> 19;
+	        
+		int regB = state.mem[state.pc] & 0x70000;
+
+		regB = regB >> 16;
+	 
+		int imm = state.mem[state.pc] & 0xFFFF;
+
+		imm = convert_num(imm);
+
+		int destR = state.mem[state.pc] & 7;
+	 
+		print_state(&state);
+	 
+		// ADD
+	      	if(optCode == 0){
+			state.reg[destR] = state.reg[regA] + state.reg[regB];
+	      		state.pc = state.pc +1;
+		}
+		// NAND
+	      	else if(optCode == 1){
+	      	printf("regA: %d, regB: %d\n", state.reg[regA], state.reg[regB]);
+	      	int regAaB = state.reg[regA] & state.reg[regB];
+	      	printf("regA&regB: %d\n", regAaB);
+			state.reg[destR] =~( state.reg[regA] & state.reg[regB]);
+	      		state.pc = state.pc +1;
+		}
+	
+
+		// LW
+	      	else if(optCode == 2){
+			state.reg[regA] = state.mem[state.reg[regB] + imm];
+	      		state.pc = state.pc +1;
+		}
+		// SW
+	      	else if(optCode == 3){
+			state.mem[state.reg[regB] + imm] = state.reg[regA]; 
+	      		state.pc = state.pc +1;
+		}
+		// BEQ
+	      	else if(optCode == 4){
+			if (state.reg[regA] == state.reg[regB]){
+				state.pc = state.pc +  imm;
+			}
+	      		state.pc = state.pc +1;
+		}
+		// JALR
+	      	else if(optCode == 5){
+			state.reg[regA] = state.pc +1;
+			state.pc = regB;
+			state.pc = state.pc +1;
+	      	}
+		// HALT
+	      	else if(optCode == 6){
+			
+			haltFlag = 1;
+	      	}
+		// NOOP
+	      	else if(optCode == 7){
+			state.pc = state.pc +1;
+	      	}
+		n_instrs ++;
+	    	}//while
+    	// Close files as apropriate
+	fclose(inFile);
+	// Print apropriate information
+    	print_state(&state);
+	printf("machine halted\n");
+	print_stats(n_instrs);
+
+	return 0;
+}//main
+
+
+int convert_num(int num){
+	if (num & (1<<15) ) {
+		num -= (1<<16);
+	}
+	return(num);
+}
+
+void print_state(statetype *stateptr){
+	int i;
+	printf("\n@@@\nstate:\n");
+	printf("\tpc %d\n", stateptr->pc);
+	printf("\tmemory:\n");
+	for(i = 0; i < stateptr->num_memory; i++){
+		printf("\t\tmem[%d]=%d\n", i, stateptr->mem[i]);
+	}
+	printf("\tregisters:\n");
+	for(i = 0; i < 8; i++){
+		printf("\t\treg[%d]=%d\n", i, stateptr->reg[i]);
+	}
+	printf("end state\n");
+}
+
+void print_stats(int n_instrs){
+	printf("INSTRUCTIONS: %d\n", n_instrs); // total executed instructions
+}
+
+	
